8d39e2959f04c8272ae3e0e8f1225b6817fff8fb
nasrinkhodayari <nasrin.khodayari@gmail.com>
2017-07-30 09:54:12 +0430

    test

--- main.js
+++ main.js
@@ -7,5 +7,5 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/static'));        // Static files
 
 server.on('request', app);
-
+ 
 server.listen(8050, function () {console.log('GRID ....' + server.address().port);});
