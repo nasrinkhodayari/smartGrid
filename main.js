8946a99f74e2b1b929270fb7e72a059c7f648a76
nasrin <nasrin.khodayari@gmail.com>
2017-08-22 15:19:50 +0430

    no message

--- main.js
+++ main.js
@@ -9,8 +9,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/static'));        // Static files
 
 server.on('request', app);
-
 // server.listen(8888, '192.168.88.49');
-server.listen(8881, function () { console.log('Wifi ....' + server.address().port); });
+
+server.listen(8881, function () {console.log('GRID ....' + server.address().port);});
 // Console will print the message
 console.log('Server running at http://192.168.88.49:8881/');
